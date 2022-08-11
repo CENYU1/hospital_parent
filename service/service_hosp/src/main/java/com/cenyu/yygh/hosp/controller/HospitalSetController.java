953d02382243744bacce3b03f40d9a926237758e
cenyu10 <kcenyu10@126.com>
2022-08-11 17:32:43 +0800

    完成了统一结果集封装，并测试成功

--- service/service_hosp/src/main/java/com/cenyu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/cenyu/yygh/hosp/controller/HospitalSetController.java
@@ -1,5 +1,6 @@
 package com.cenyu.yygh.hosp.controller;
 
+import com.cenyu.yygh.common.result.Result;
 import com.cenyu.yygh.hosp.service.HospitalSetService;
 import com.cenyu.yygh.model.hosp.HospitalSet;
 import io.swagger.annotations.Api;
@@ -18,13 +19,16 @@ public class HospitalSetController {
 
     @ApiOperation(value = "获取所有医院设置")
     @GetMapping("/findAll")
-    public List<HospitalSet> findAllHospitalSet() {
-        return hospitalSetService.list();
+    public Result findAllHospitalSet() {
+        List<HospitalSet> data = hospitalSetService.list();
+        return Result.ok(data);
     }
 
     @ApiOperation(value = "逻辑删除医院设置")
     @DeleteMapping("/delete/{id}")
-    public boolean removeHospitalSet(@PathVariable Long id) {
-        return hospitalSetService.removeById(id);
+    public Result removeHospitalSet(@PathVariable Long id) {
+        boolean flag = hospitalSetService.removeById(id);
+        if(flag) return Result.ok();
+        else return Result.fail();
     }
 }

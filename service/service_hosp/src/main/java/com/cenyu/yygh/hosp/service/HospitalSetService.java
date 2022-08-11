e86c8db8a46e15b4bd204291433b1c2866216b6e
cenyu10 <kcenyu10@126.com>
2022-08-11 15:42:20 +0800

    完成医院设置模块的CRUD基础模式

---
+++ service/service_hosp/src/main/java/com/cenyu/yygh/hosp/service/HospitalSetService.java
@@ -0,0 +1,7 @@
+package com.cenyu.yygh.hosp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.cenyu.yygh.model.hosp.HospitalSet;
+
+public interface HospitalSetService extends IService<HospitalSet> {
+}
---
+++ service/service_hosp/src/main/java/com/cenyu/yygh/hosp/service/impl/HospitalSetServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cenyu.yygh.hosp.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cenyu.yygh.hosp.mapper.HospitalSetMapper;
+import com.cenyu.yygh.hosp.service.HospitalSetService;
+import com.cenyu.yygh.model.hosp.HospitalSet;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper, HospitalSet> implements HospitalSetService {
+}

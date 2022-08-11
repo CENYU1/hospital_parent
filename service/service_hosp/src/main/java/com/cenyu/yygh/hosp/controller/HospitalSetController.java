4ad4cb11eca25708f4b1064672c4bc8bb92e84cc
cenyu10 <kcenyu10@126.com>
2022-08-11 18:23:49 +0800

    完成了条件查询带分页功能

--- service/service_hosp/src/main/java/com/cenyu/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/cenyu/yygh/hosp/controller/HospitalSetController.java
@@ -1,11 +1,15 @@
 package com.cenyu.yygh.hosp.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cenyu.yygh.common.result.Result;
 import com.cenyu.yygh.hosp.service.HospitalSetService;
 import com.cenyu.yygh.model.hosp.HospitalSet;
+import com.cenyu.yygh.vo.hosp.HospitalSetQueryVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,4 +35,20 @@ public class HospitalSetController {
         if(flag) return Result.ok();
         else return Result.fail();
     }
+
+    @ApiOperation(value = "条件查询带分页")
+    @PostMapping("/findPageHospSet/{current}/{limit}")
+    public Result findPageHospSet(@PathVariable Long current,
+                                  @PathVariable Long limit,
+                                  @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+        Page<HospitalSet> page = new Page<>(current, limit);
+        QueryWrapper<HospitalSet> queryWrapper = new QueryWrapper<>();
+        String hosname = hospitalSetQueryVo.getHosname();
+        String hoscode = hospitalSetQueryVo.getHoscode();
+        if(!StringUtils.isEmpty(hosname)) queryWrapper.like("hosname", hosname);
+        if(!StringUtils.isEmpty(hoscode)) queryWrapper.eq("hoscode", hoscode);
+        Page<HospitalSet> pageHospitalSet = hospitalSetService.page(page, queryWrapper);
+        return Result.ok(pageHospitalSet);
+    }
+
 }
